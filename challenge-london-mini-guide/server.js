a1c46dbf0a0c84ad5752ed4410978c0e3c417306
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-06-06 12:49:02 +0100

    added gitignore

--- challenge-london-mini-guide/server.js
+++ challenge-london-mini-guide/server.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const app = express();
+
+const PORT = process.env.PORT || 3003;
+
+// app.use(express.urlencoded({ extended: true }));\
+
+app.listen(PORT, (req, res) => console.log(`App is working on ${PORT}`));
