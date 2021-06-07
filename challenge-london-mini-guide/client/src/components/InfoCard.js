c07ff2b933d9c9fbb68ddc731ac7cdc36b1a2159
BenyamWorku <benworku@gmail.com>
2021-06-07 22:37:52 +0100

    made some css changes

--- challenge-london-mini-guide/client/src/components/InfoCard.js
+++ challenge-london-mini-guide/client/src/components/InfoCard.js
@@ -5,11 +5,11 @@ const InfoCard = ({ info }) => {
 	return (
 		<div className='info-card'>
 			<h4>{info.name}</h4>
-			<a href={`${info.website}`} target='_blank' rel="noreferrer">{info.website}</a>
+			<a href={`${info.website}`} target='_blank' rel="noreferrer">link to website</a>
 			<p>Phone Number: {info.phone}</p>
 			<p>Address: {info.address}</p>
 		</div>
 	)
 }
-
+// {info.website}
 export default InfoCard;
