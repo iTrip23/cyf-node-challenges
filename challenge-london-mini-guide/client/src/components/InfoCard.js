2d765fefa1f15464f7926e688a5378c96d20e530
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-06-07 22:33:35 +0100

    added a bit of bootstrap

--- challenge-london-mini-guide/client/src/components/InfoCard.js
+++ challenge-london-mini-guide/client/src/components/InfoCard.js
@@ -3,11 +3,13 @@ import "./InfoCard.css"
 
 const InfoCard = ({ info }) => {
 	return (
-		<div className='info-card'>
-			<h4>{info.name}</h4>
-			<a href={`${info.website}`} target='_blank' rel="noreferrer">{info.name}</a>
-			<p>Phone Number: {info.phone}</p>
-			<p>Address: {info.address}</p>
+		<div className="card col-3 h-100 card-info">
+			<div className="card-body">
+				<h5 className="card-title">{info.name}</h5>
+				<p className="card-text">Phone Number: {info.phone}</p>
+				<p className="card-text">Address: {info.address}</p>
+				<a href={`${info.website}`} className="card-link" target='_blank' rel="noreferrer">{info.name}</a>
+			</div>
 		</div>
 	)
 }
