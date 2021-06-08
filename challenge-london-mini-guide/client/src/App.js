73d1449d9704877eccd7b5ee50b75ac10fa138a8
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-06-08 22:38:44 +0100

    reverted to previous change

--- challenge-london-mini-guide/client/src/App.js
+++ challenge-london-mini-guide/client/src/App.js
@@ -1,62 +1,48 @@
-import "./App.css";
-import { useState, useEffect } from "react";
-import Button from "./components/Button.js";
-import InfoCardContainer from "./components/InfoCardContainer.js";
+import './App.css';
+import { useState, useEffect } from 'react';
+import Button from './components/Button.js';
+import InfoCardContainer from './components/InfoCardContainer.js';
 
 function App() {
   const [cityData, setCityData] = useState(null);
   const [boroughInfo, setBoroughInfo] = useState(null);
-  const [cityName, setCityName] = useState("harrow");
+  const [cityName, setCityName] = useState('harrow')
 
-  const allCities = ["harrow", "stratford", "heathrow"];
+  const allCities = ['harrow', 'stratford', 'heathrow'];
 
   const getData = async (cityName) => {
-    const response = await fetch(`http://localhost:3003/${cityName}`);
+    const response = await fetch(`http://localhost:3003/${cityName}`)
     const data = await response.json();
     setCityData(data);
-  };
+  }
 
   useEffect(() => {
     try {
-      getData(cityName);
+      getData(cityName)
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
-  }, [cityName]);
+  }, [cityName])
 
   return (
     <div className="App">
-      <div className="dropdown">
-        <select
-          onChange={(e) => {
-            setCityData(e.target.value);
-            setCityName(e.target.value);
-          }}
-        >
-          {allCities.map((elem, index) => (
-            <option key={index} value={elem}>
-              {elem}
-            </option>
-          ))}
+
+      <div className='dropdown'>
+        <select onChange={(e) => {
+          setCityData(e.target.value)
+          setCityName(e.target.value)
+        }}>
+          {allCities.map((elem, index) => <option key={index} value={elem}>{elem}</option>)}
         </select>
       </div>
 
-      <div className="button-container">
-        {cityData === null ? (
-          <h1>Loading...</h1>
-        ) : (
-          Object.keys(cityData).map((elem, index) => (
-            <Button
-              name={elem}
-              key={index}
-              setBoroughInfo={setBoroughInfo}
-              cityName={cityName}
-            />
-          ))
-        )}
+      <div className='button-container'>
+        {cityData === null ? <h1>Loading...</h1> : Object.keys(cityData).map((elem, index) => <Button
+          name={elem} key={index} setBoroughInfo={setBoroughInfo} cityName={cityName} />)}
       </div>
 
       <InfoCardContainer boroughInfo={boroughInfo} />
+
     </div>
   );
 }
--- challenge-london-mini-guide/client/src/components/InfoCard.js
+++ challenge-london-mini-guide/client/src/components/InfoCard.js
@@ -3,28 +3,13 @@ import "./InfoCard.css"
 
 const InfoCard = ({ info }) => {
 	return (
-<<<<<<< HEAD
 		<div className='info-card'>
 			<h4>{info.name}</h4>
-			<a href={`${info.website}`} target='_blank' rel="noreferrer">link to website</a>
+			<a href={`${info.website}`} target='_blank' rel="noreferrer">{info.name}</a>
 			<p>Phone Number: {info.phone}</p>
 			<p>Address: {info.address}</p>
 		</div>
 	)
 }
-// {info.website}
-export default InfoCard;
-=======
-		<div className="card col-3 h-100 card-info">
-			<div className="card-body">
-				<h5 className="card-title">{info.name}</h5>
-				<p className="card-text">Phone Number: {info.phone}</p>
-				<p className="card-text">Address: {info.address}</p>
-				<a href={`${info.website}`} className="card-link" target='_blank' rel="noreferrer">{info.name}</a>
-			</div>
-		</div>
-	)
-}
 
 export default InfoCard;
->>>>>>> 2d765fefa1f15464f7926e688a5378c96d20e530
